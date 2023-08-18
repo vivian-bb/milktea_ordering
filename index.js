b953dcfa5ecdfacbf26230281e146d32e96207ae
vivian-bb <73925845+vivian-bb@users.noreply.github.com>
2023-08-18 13:59:00 +1000

    Javascript work - get value from form in html

--- index.js
+++ index.js
@@ -1,19 +1,26 @@
-const form = document.querySelector('#order-form');
-const formData = new FormData(form);
+    const formData = new FormData(document.getElementById('orderForm'));
+    const type = formData.get('type');
+    const num = formData.get('num');
+    const size = formData.get('size');
+    const suger = formData.get('suger');
+    const snakeFree = formData.get('snake-free');
+    const snake = formData.get('snake');
+    const ice = formData.get('ice');
+    const address = formData.get('address');
+    const phone = formData.get('phone');
+    const time = formData.get('time');
+    const notes = formData.get('notes');
+    const payType = formData.get('pay-type');
 
-// 获取表单数据中的 name 和 age 字段
-const type = formData.get('type');
-const num = formData.get('num');
-const size = formData.get('size');
-const snake-free = formData.get('snake-free');
-const snake = formData.get('snake');
-const ice = formData.get('ice');
-const address = formData.get('address');
-const phone = formData.get('phone');
-const time = formData.get('time');
-const notes = formData.get('notes');
-const pay-type = formData.get('pay-type');
-
-
-// 将表单数据转换为 JSON 字符串
-const formJson = JSON.stringify(Object.fromEntries(formData.entries()));
+    // const formJson = JSON.stringify(Object.fromEntries(formData.entries()));
+    console.log('Milktea Flaver: ' + type );
+    console.log('Quantity: ' + num );
+    console.log('Size: ' + size );
+    console.log('Suger: ' + suger)
+    console.log('Topping: ' + snakeFree );
+    console.log('Additional Options: ' + snake );
+    console.log('Ice: ' + ice );
+    console.log('Delivery address: ' + address );
+    console.log('Expected Delivery time: ' + phone );
+    console.log('Notes: ' + phone );
+    console.log('Payment method: ' + phone );
