73c1fba72096e3152d0a78b5daab72d6775ef602
vivian-bb <73925845+vivian-bb@users.noreply.github.com>
2023-08-21 17:28:01 +1000

    submit button and scrollup btn

--- index.js
+++ index.js
@@ -1,26 +1,50 @@
-    const formData = new FormData(document.getElementById('orderForm'));
-    const type = formData.get('type');
-    const num = formData.get('num');
-    const size = formData.get('size');
-    const suger = formData.get('suger');
-    const snakeFree = formData.get('snake-free');
-    const snake = formData.get('snake');
-    const ice = formData.get('ice');
-    const address = formData.get('address');
-    const phone = formData.get('phone');
-    const time = formData.get('time');
-    const notes = formData.get('notes');
-    const payType = formData.get('pay-type');
 
-    // const formJson = JSON.stringify(Object.fromEntries(formData.entries()));
-    console.log('Milktea Flaver: ' + type );
-    console.log('Quantity: ' + num );
-    console.log('Size: ' + size );
-    console.log('Suger: ' + suger)
-    console.log('Topping: ' + snakeFree );
-    console.log('Additional Options: ' + snake );
-    console.log('Ice: ' + ice );
-    console.log('Delivery address: ' + address );
-    console.log('Expected Delivery time: ' + phone );
-    console.log('Notes: ' + phone );
-    console.log('Payment method: ' + phone );
+const form = document.getElementById('orderForm');
+const btnOrder = document.getElementById('button1')
+const scrollToTopBtn =document.querySelector(".scroll-to-top");
+const getOrderDetails = (formDetails) => {
+    const text = `
+        [Your order had been sent]
+        --------------------------------
+        Flavour: ${formDetails.type}
+        Amount: ${formDetails.num}
+        Size: ${formDetails.size}
+        Suger lever: ${formDetails.suger}
+        Topping: ${formDetails["snake-free"] ? formDetails["snake-free"] : "-"}
+        Additional Option: ${formDetails["additional"] ? formDetails["additional"] : "-"}
+        Ice level: ${formDetails.ice}
+        Address: ${formDetails.address}
+        Phone Number: ${formDetails.phone}
+        Expected Delivery Time: ${formDetails.time}
+        Notes: ${formDetails.notes}
+        Payment Method: ${formDetails["pay-type"]}
+
+    
+    `;
+    return text;
+}
+
+const onSubmit =(e)=>{
+    e.preventDefault();
+
+    const formData = new FormData(form);
+    const formVals ={};
+    for (var pair of formData.entries()){
+        const propName = pair[0];
+        const propValue = pair[1];
+
+        if(formVals[propName]) {
+            formVals[propName] = [propValue].concat(formVals[propName]);
+        } else {
+            formVals[propName] = propValue;
+        }
+    }
+console.log(formVals)
+alert(getOrderDetails(formVals));
+};
+
+
+form.addEventListener('submit', onSubmit);
+scrollToTopBtn.addEventListener('click', ()=>{
+    document.documentElement.scrollTop = 0;
+})
